4055bec7585d46a72103810d6cbc8e332a8ba86a
Matheus Pessino <paulomatheuspessino@gmail.com>
2022-07-20 09:17:03 -0300

    Criação da estrutura basica e atributos

---
+++ Delta.java
@@ -0,0 +1,16 @@
+public class Delta extends Equacao {
+    
+    double delta;
+
+    public double getDelta() {
+        return delta;
+    }
+
+    public void setDelta(double delta) {
+        this.delta = delta;
+    }
+
+    public Delta calculaDelta{
+
+    }
+}
---
+++ EquacaoX.java
@@ -0,0 +1,25 @@
+public class EquacaoX {
+    private double xn = 0;
+    private double xp = 0;
+    public double getXn() {
+        return xn;
+    }
+    public void setXn(double xn) {
+        this.xn = xn;
+    }
+    public double getXp() {
+        return xp;
+    }
+    public void setXp(double xp) {
+        this.xp = xp;
+    }
+
+    public double calculaXn(double delta){
+
+    }
+
+    public double calculaXp(double delta){
+
+    }
+    
+}
